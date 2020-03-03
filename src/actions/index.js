6f8d154f15487bd9b192eb7e55bdd5defc8835c8
unknown <fnumilat@gmail.com>
2020-03-02 22:17:03 -0600

    work done

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const ADD_FEATURE = 'ADD_FEATURE'
+export const addFeature = (feature) => {
+    return {type: ADD_FEATURE, payload: feature};
+};
+
+export const REMOVE_FEATURE = 'REMOVE_FEATURE'
+export const remove = (id) => {
+    return {type: REMOVE_FEATURE, payload: id};
+};
